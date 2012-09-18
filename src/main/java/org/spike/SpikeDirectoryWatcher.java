52084d0e6a4ee4c949326705aceb2e934e5acef2
mikomatic <mikomatic@mac>
2012-09-18 09:30:07 +0200

    fixes when relaunching process

--- src/main/java/org/spike/SpikeDirectoryWatcher.java
+++ src/main/java/org/spike/SpikeDirectoryWatcher.java
@@ -20,150 +20,148 @@ import freemarker.template.TemplateException;
 
 /**
  * @author mikomatic
- *
+ * 
  */
 public class SpikeDirectoryWatcher extends TimerTask implements FilenameFilter {
 
-    private static Logger log = Logger.getLogger( SpikeDirectoryWatcher.class.getName() );
+	private static Logger log = Logger.getLogger(SpikeDirectoryWatcher.class.getName());
 
-    private static Spike spike;
-    private File filesArray[];
-    private final Map<String,Long> dir = new HashMap<String,Long>();
+	private static Spike spike;
+	private File filesArray[];
+	private final Map<String, Long> dir = new HashMap<String, Long>();
 
-    private enum Action {
-        ADD, DELETE, MODIFY;
-    }
+	private enum Action {
+		ADD, DELETE, MODIFY;
+	}
 
-    private class FileAction {
+	private class FileAction {
 
-        private final Action action;
+		private final Action action;
 
-        private final String fileName;
+		private final String fileName;
 
-        public FileAction( Action pAction, String pFileName ) {
-            super();
-            action = pAction;
-            fileName = pFileName;
-        }
+		public FileAction(Action pAction, String pFileName) {
+			super();
+			action = pAction;
+			fileName = pFileName;
+		}
 
-        public Action getAction() {
-            return action;
-        }
+		public Action getAction() {
+			return action;
+		}
 
-        public String getFileName() {
-            return fileName;
-        }
-    }
+		public String getFileName() {
+			return fileName;
+		}
+	}
 
-    public SpikeDirectoryWatcher( Spike pSpike ) {
-        spike = pSpike;
-        List<File> listsfiles = FileUtils.listFiles( spike.getSourcePath(), this );
-        filesArray = listsfiles.toArray( new File[ listsfiles.size() ] );
+	public SpikeDirectoryWatcher(Spike pSpike) {
+		spike = pSpike;
+		List<File> listsfiles = FileUtils.listFiles(spike.getSourcePath(), this);
+		filesArray = listsfiles.toArray(new File[listsfiles.size()]);
 
-        for ( File lFile : filesArray ) {
-            dir.put( lFile.getAbsolutePath(), new Long( lFile.lastModified() ) );
-        }
-    }
+		for (File lFile : filesArray) {
+			dir.put(lFile.getAbsolutePath(), new Long(lFile.lastModified()));
+		}
+	}
 
-    @Override
-    public final void run() {
-        HashSet<String> checkedFiles = new HashSet<String>();
+	@Override
+	public final void run() {
+		HashSet<String> checkedFiles = new HashSet<String>();
 
-        Map<Action,List<String>> pFileModMap = new HashMap<SpikeDirectoryWatcher.Action,List<String>>();
-        pFileModMap.put( Action.ADD, new ArrayList<String>() );
-        pFileModMap.put( Action.DELETE, new ArrayList<String>() );
-        pFileModMap.put( Action.MODIFY, new ArrayList<String>() );
+		Map<Action, List<String>> pFileModMap = new HashMap<SpikeDirectoryWatcher.Action, List<String>>();
+		pFileModMap.put(Action.ADD, new ArrayList<String>());
+		pFileModMap.put(Action.DELETE, new ArrayList<String>());
+		pFileModMap.put(Action.MODIFY, new ArrayList<String>());
 
-        List<FileAction> fileActionList = new ArrayList<SpikeDirectoryWatcher.FileAction>();
-        // Scan the files and check for modification/addition
-        for ( File lFile : filesArray ) {
+		List<FileAction> fileActionList = new ArrayList<SpikeDirectoryWatcher.FileAction>();
+		// Scan the files and check for modification/addition
+		for (File lFile : filesArray) {
 
-            String lFilePathName = lFile.getAbsolutePath();
-            Long current = dir.get( lFilePathName );
-            if ( current == null ) {
-                // new file
-                dir.put( lFilePathName, new Long( lFile.lastModified() ) );
-                fileActionList.add( new FileAction( Action.ADD, lFilePathName ) );
-                break;
-            }
-            else if ( current.longValue() != lFile.lastModified() ) {
-                // modified file
-                dir.put( lFilePathName, new Long( lFile.lastModified() ) );
-                fileActionList.add( new FileAction( Action.MODIFY, lFilePathName ) );
-                break;
-            }
-            checkedFiles.add( lFilePathName );
-        }
+			String lFilePathName = lFile.getAbsolutePath();
+			Long current = dir.get(lFilePathName);
+			if (current == null) {
+				// new file
+				dir.put(lFilePathName, new Long(lFile.lastModified()));
+				fileActionList.add(new FileAction(Action.ADD, lFilePathName));
+				break;
+			} else if (current.longValue() != lFile.lastModified()) {
+				// modified file
+				dir.put(lFilePathName, new Long(lFile.lastModified()));
+				fileActionList.add(new FileAction(Action.MODIFY, lFilePathName));
+				break;
+			}
+			checkedFiles.add(lFilePathName);
+		}
 
-        if ( !fileActionList.isEmpty() ) {
-            if ( fileActionList.size() == 1 ) {
-                onChange( fileActionList.get( 0 ).getFileName(), fileActionList.get( 0 ).getAction().name() );
-            }
-            else {
-                onChanges();
-            }
-        }
+		if (!fileActionList.isEmpty()) {
+			if (fileActionList.size() == 1) {
+				onChange(fileActionList.get(0).getFileName(), fileActionList.get(0).getAction()
+						.name());
+			} else {
+				onChanges();
+			}
+		}
 
-        //TODO handle file delete
-        // HashSet<String> lAllFiles = new HashSet<String>(dir.keySet());
-        // lAllFiles.removeAll(checkedFiles);
-        // // If All is not null, this means a files has been deleted
-        // Iterator<String> it = lAllFiles.iterator();
-        // while (it.hasNext()) {
-        // String lDeletedPathName = it.next();
-        // dir.remove(lDeletedPathName);
-        // onChange(lDeletedPathName, "delete");
-        // break;
-        // }
+		// TODO handle file delete
+		// HashSet<String> lAllFiles = new HashSet<String>(dir.keySet());
+		// lAllFiles.removeAll(checkedFiles);
+		// // If All is not null, this means a files has been deleted
+		// Iterator<String> it = lAllFiles.iterator();
+		// while (it.hasNext()) {
+		// String lDeletedPathName = it.next();
+		// dir.remove(lDeletedPathName);
+		// onChange(lDeletedPathName, "delete");
+		// break;
+		// }
 
-    }
+	}
 
-    protected void onChanges() {
-        System.out.println( "I don't know how to handle several changes at once...yet" );
-    }
+	protected void onChanges() {
+		System.out.println("I don't know how to handle several changes at once...yet");
+	}
 
-    protected void onChange( String pFilePathName, String pAction ) {
-        log.info( "A File change has been detected\n " + "File Path : " + pFilePathName + " Action: " + pAction );
+	protected void onChange(String pFilePathName, String pAction) {
+		log.info("A File change has been detected\n " + "File Path : " + pFilePathName
+				+ " Action: " + pAction);
 
-        try {
-            // C'est un fichier lié au process. Il faut le relance
-            if ( pFilePathName.replace( spike.getSourcePath() + File.separator, "" ).startsWith( "_" ) ) {
-                System.out.println( "Relaunching spike process...please wait" );
-                spike.runProcess();
-            }
-            // Sinon (eg. fichier d'affichage: css, js)
-            else {
-                spike.copySource();
-            }
-        }
-        catch ( IOException e ) {
-            log.severe( e.getMessage() );
-        }
-        catch ( TemplateException e ) {
-            log.severe( e.getMessage() );
-        }
-        System.out.println( "Hit any key yo stop..." );
-    }
+		try {
+			// C'est un fichier liï¿½ au process. Il faut le relance
+			if (pFilePathName.replace(spike.getSourcePath() + File.separator, "").startsWith("_")) {
+				System.out.println("Relaunching spike process...please wait");
+				spike.runProcess();
+			}
+			// Sinon (eg. fichier d'affichage: css, js)
+			else {
+				spike.copySource();
+			}
+		} catch (IOException e) {
+			log.severe(e.getMessage());
+		} catch (TemplateException e) {
+			log.severe(e.getMessage());
+		}
+		System.out.println("Hit any key yo stop...");
+	}
 
-    @Override
-    public boolean accept( File pDir, String pName ) {
-        //Si le dossier source et output sont les mêmes: on ne scanne que les dossiers source
-        if ( spike.isSameInputOutPut() ) {
-            if ( pName.startsWith( "_" ) ) {
-                return true;
-            }
-            if ( pDir.getName().startsWith( "_" ) ) {
-                return true;
-            }
-            return false;
-        }
-        else {
-            //On ne scanne jamais le fichier output qui risque d'être modifié souvent
-            if ( pDir.getAbsolutePath().equals( spike.getOutput() ) ) {
-                return false;
-            }
-            return !pName.startsWith( "." ) ? true : false;
-        }
-    }
+	public boolean accept(File pDir, String pName) {
+		// Si le dossier source et output sont les mï¿½mes: on ne scanne que les
+		// dossiers source
+		if (spike.isSameInputOutPut()) {
+			if (pName.startsWith("_")) {
+				return true;
+			}
+			if (pDir.getName().startsWith("_")) {
+				return true;
+			}
+			return false;
+		} else {
+			// On ne scanne jamais le fichier output qui risque d'ï¿½tre modifiï¿½
+			// souvent
+			if (pDir.getAbsolutePath().equals(spike.getOutput())) {
+				return false;
+			}
+			return !pName.startsWith(".") ? true : false;
+		}
+	}
 
 }
--- src/main/java/org/spike/model/Navigation.java
+++ src/main/java/org/spike/model/Navigation.java
@@ -20,81 +20,93 @@ import org.spike.model.navigation.PostNav;
  */
 public class Navigation {
 
-    private static final SimpleDateFormat monthFormat = new SimpleDateFormat( "MMMMMMMM", Locale.FRENCH );
-    private static final SimpleDateFormat dayOfWeekFormat = new SimpleDateFormat( "EEEEEEEE, d", Locale.FRENCH );
-    private static Map<String,Map<String,Set<PostArchive>>> archiveMap = new LinkedHashMap<String,Map<String,Set<PostArchive>>>();
+	private static final SimpleDateFormat monthFormat = new SimpleDateFormat("MMMMMMMM",
+			Locale.FRENCH);
+	private static final SimpleDateFormat dayOfWeekFormat = new SimpleDateFormat("EEEEEEEE, d",
+			Locale.FRENCH);
+	private static Map<String, Map<String, Set<PostArchive>>> archiveMap = new LinkedHashMap<String, Map<String, Set<PostArchive>>>();
 
-    private static Map<String,Set<PostNav>> categoriesMap = new LinkedHashMap<String,Set<PostNav>>();
-    private static Map<String,Set<PostNav>> tagsMap = new LinkedHashMap<String,Set<PostNav>>();
+	private static Map<String, Set<PostNav>> categoriesMap = new LinkedHashMap<String, Set<PostNav>>();
+	private static Map<String, Set<PostNav>> tagsMap = new LinkedHashMap<String, Set<PostNav>>();
 
-    private Navigation() {
-        // No instance
-    }
+	private Navigation() {
+		// No instance
+	}
 
-    public static void add( final Post pPost ) {
-        Calendar publishedCal = pPost.getPublishedCal();
-        String lYear = String.valueOf( publishedCal.get( Calendar.YEAR ) );
-        String lMonth = monthFormat.format( pPost.getPublishedDate() );
-        String lDayOfMonth = dayOfWeekFormat.format( pPost.getPublishedDate() );
+	public static void clear() {
+		archiveMap.clear();
+		categoriesMap.clear();
+		tagsMap.clear();
+	}
 
-        // Fill Archive Map
-        Map<String,Set<PostArchive>> lYearMap = archiveMap.get( lYear );
-        if ( lYearMap == null ) {
-            lYearMap = new LinkedHashMap<String,Set<PostArchive>>();
-            archiveMap.put( lYear, lYearMap );
-        }
+	public static void add(final Post pPost) {
+		Calendar publishedCal = pPost.getPublishedCal();
+		String lYear = String.valueOf(publishedCal.get(Calendar.YEAR));
+		String lMonth = monthFormat.format(pPost.getPublishedDate());
+		String lDayOfMonth = dayOfWeekFormat.format(pPost.getPublishedDate());
 
-        Set<PostArchive> lMonthList = archiveMap.get( lYear ).get( lMonth );
-        if ( lMonthList == null ) {
-            lMonthList = new LinkedHashSet<PostArchive>();
-            archiveMap.get( lYear ).put( lMonth, lMonthList );
-        }
+		// Fill Archive Map
+		Map<String, Set<PostArchive>> lYearMap = archiveMap.get(lYear);
+		if (lYearMap == null) {
+			lYearMap = new LinkedHashMap<String, Set<PostArchive>>();
+			archiveMap.put(lYear, lYearMap);
+		}
 
-        PostArchive lPostArchive = new PostArchive( pPost.getTitle(), pPost.getUrl(), lYear, lMonth, lDayOfMonth );
-        archiveMap.get( lYear ).get( lMonth ).add( lPostArchive );
+		Set<PostArchive> lMonthList = archiveMap.get(lYear).get(lMonth);
+		if (lMonthList == null) {
+			lMonthList = new LinkedHashSet<PostArchive>();
+			archiveMap.get(lYear).put(lMonth, lMonthList);
+		}
 
-        // Fill Category Map
-        String lCategory = pPost.getCategory() != null ? pPost.getCategory().toLowerCase() : "no-category";
-        Set<PostNav> lPostByCat = categoriesMap.get( lCategory );
-        if ( lPostByCat == null ) {
-            lPostByCat = new LinkedHashSet<PostNav>();
-            categoriesMap.put( lCategory, lPostByCat );
-        }
-        categoriesMap.get( lCategory ).add( new PostNav( pPost.getTitle(), pPost.getUrl(), pPost.getPublishedDate() ) );
+		PostArchive lPostArchive = new PostArchive(pPost.getTitle(), pPost.getUrl(), lYear, lMonth,
+				lDayOfMonth);
+		archiveMap.get(lYear).get(lMonth).add(lPostArchive);
 
-        // Fill Category Map
-        List<String> lTags = pPost.getTags();
-        for ( String lTag : lTags ) {
-            if ( lTag != null && lTag.trim().length() != 0 ) {
-                Set<PostNav> lPostByTag = tagsMap.get( lTag );
-                if ( lPostByTag == null ) {
-                    lPostByTag = new LinkedHashSet<PostNav>();
-                    tagsMap.put( lTag, lPostByTag );
-                }
-                tagsMap.get( lTag ).add( new PostNav( pPost.getTitle(), pPost.getUrl(), pPost.getPublishedDate() ) );
-            }
-        }
+		// Fill Category Map
+		String lCategory = pPost.getCategory() != null ? pPost.getCategory().toLowerCase()
+				: "no-category";
+		Set<PostNav> lPostByCat = categoriesMap.get(lCategory);
+		if (lPostByCat == null) {
+			lPostByCat = new LinkedHashSet<PostNav>();
+			categoriesMap.put(lCategory, lPostByCat);
+		}
+		categoriesMap.get(lCategory).add(
+				new PostNav(pPost.getTitle(), pPost.getUrl(), pPost.getPublishedDate()));
 
-    }
+		// Fill Category Map
+		List<String> lTags = pPost.getTags();
+		for (String lTag : lTags) {
+			if (lTag != null && lTag.trim().length() != 0) {
+				Set<PostNav> lPostByTag = tagsMap.get(lTag);
+				if (lPostByTag == null) {
+					lPostByTag = new LinkedHashSet<PostNav>();
+					tagsMap.put(lTag, lPostByTag);
+				}
+				tagsMap.get(lTag).add(
+						new PostNav(pPost.getTitle(), pPost.getUrl(), pPost.getPublishedDate()));
+			}
+		}
 
-    @Deprecated
-    public void buildArchiveFromList( List<Post> pPosts ) {
-        archiveMap.clear();
+	}
 
-        for ( Post lPost : pPosts ) {
-            add( lPost );
-        }
-    }
+	@Deprecated
+	public void buildArchiveFromList(List<Post> pPosts) {
+		archiveMap.clear();
 
-    public static Map<String,Map<String,Set<PostArchive>>> getArchiveMap() {
-        return archiveMap;
-    }
+		for (Post lPost : pPosts) {
+			add(lPost);
+		}
+	}
 
-    public static Map<String,Set<PostNav>> getCategoriesMap() {
-        return categoriesMap;
-    }
+	public static Map<String, Map<String, Set<PostArchive>>> getArchiveMap() {
+		return archiveMap;
+	}
 
-    public static Map<String,Set<PostNav>> getTagsMap() {
-        return tagsMap;
-    };
+	public static Map<String, Set<PostNav>> getCategoriesMap() {
+		return categoriesMap;
+	}
+
+	public static Map<String, Set<PostNav>> getTagsMap() {
+		return tagsMap;
+	};
 }
